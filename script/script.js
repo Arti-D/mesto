b13e4b4547d0ce83b832048c4677a01e8d86257d
Arti <arti.cgd@yandex.ru>
2020-11-30 19:33:31 +0300

    style fixed

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-btn');
 let popup = document.querySelector('.popup');
+let name = document.querySelector('.profile__title');
+let aboutInfo = document.querySelector('.profile__subtitle');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    
 }
 
 editButton.addEventListener('click', openPopup);
@@ -14,3 +17,8 @@ function closePopup() {
 
 closeButton.addEventListener('click', closePopup);
 
+
+
+function editInfo() {
+    
+}
