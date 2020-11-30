d64945e1e186c7b32cb93e2b1944aa0574f8acea
Arti <arti.cgd@yandex.ru>
2020-11-30 21:44:44 +0300

    done

--- script/script.js
+++ script/script.js
@@ -1,24 +1,35 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-btn');
 let popup = document.querySelector('.popup');
+let nameField = document.querySelector('.popup__form_name');
+let aboutField  = document.querySelector('.popup__form_about');
 let name = document.querySelector('.profile__title');
-let aboutInfo = document.querySelector('.profile__subtitle');
+let about= document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__form');
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    
+    nameField.value = name.textContent;
+    aboutField.value = about.textContent;
 }
-
 editButton.addEventListener('click', openPopup);
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
-
 closeButton.addEventListener('click', closePopup);
 
 
+function formSubmitHandler(evt) {
+    console.log('тык');
+    evt.preventDefault();
 
-function editInfo() {
-    
+    let nameInput = nameField.value;
+    let jobInput = aboutField.value;
+
+    name.textContent = nameInput;
+    about.textContent = jobInput;
+
+    closePopup()
 }
+formElement.addEventListener('submit', formSubmitHandler);
