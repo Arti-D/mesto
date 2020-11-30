1fb07432b544697f3c91ba21fd7e07384ad82a20
Arti <arti.cgd@yandex.ru>
2020-11-30 18:47:50 +0300

    open-close popup

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-btn');
+let popup = document.querySelector('.popup');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', closePopup);
+
