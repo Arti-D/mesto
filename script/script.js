505434660ccff262b5a67479ece83b01bbf3f731
Arti <arti.cgd@yandex.ru>
2020-11-30 22:36:57 +0300

    html after validator

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-btn');
 let popup = document.querySelector('.popup');
-let nameField = document.querySelector('.popup__form_name');
-let aboutField  = document.querySelector('.popup__form_about');
+let nameField = document.querySelector('.popup__input_name');
+let aboutField  = document.querySelector('.popup__input_about');
 let name = document.querySelector('.profile__title');
 let about= document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
@@ -21,7 +21,7 @@ closeButton.addEventListener('click', closePopup);
 
 
 function formSubmitHandler(evt) {
-    
+
     evt.preventDefault();
 
     let nameInput = nameField.value;
